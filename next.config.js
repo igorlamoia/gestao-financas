bb869d00d95d22da6fee72393e46b83dec71dc54
igor lamoia <igor@hbifinanceira.com.br>
2023-04-09 11:08:57 -0300

    chore: somente .page e .api viram rotas

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: ['images.unsplash.com'],
+  },
+  pageExtensions: ['page.tsx', 'page.ts', 'api.ts', 'api.tsx'],
+};
 
 module.exports = nextConfig;
