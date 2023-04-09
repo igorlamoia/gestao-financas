24a215994ed8e3a199d38e2e2c34db11897814c2
igor lamoia <igor@hbifinanceira.com.br>
2023-04-09 16:01:04 -0300

    chore: nova sequencia de rotas

--- src/components/aside/li.tsx
+++ src/components/aside/li.tsx
@@ -1,4 +1,3 @@
-import Image from 'next/image';
 import Link from 'next/link';
 
 interface LiProps {
@@ -7,9 +6,10 @@ interface LiProps {
   children: React.ReactNode;
   active?: boolean;
   path: string | null;
+  svg?: React.ReactNode;
 }
 
-export function Li({ href, imagePath, path, children }: LiProps) {
+export function Li({ href, imagePath, path, children, svg }: LiProps) {
   const active = path === href;
   return (
     <li className="relative">
@@ -20,9 +20,27 @@ export function Li({ href, imagePath, path, children }: LiProps) {
         ${active ? 'before:block text-purple-500' : 'before:hidden'}
         `}
       >
-        <div className="w-[90px] flex justify-center">
-          <Image src="/bank.svg" alt="Logo" width={25} height={25} />
+        <div className="w-[90px] flex justify-center align-middle">
+          {svg ? (
+            svg
+          ) : (
+            <svg
+              className="block h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth="1.5"
+              stroke="currentColor"
+              aria-hidden="true"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+              />
+            </svg>
+          )}
         </div>
+
         {children}
       </Link>
     </li>
--- src/components/aside/navbar.tsx
+++ src/components/aside/navbar.tsx
@@ -6,8 +6,28 @@ import { Li } from './li';
 
 const links = [
   {
-    href: '/dashboard/principal',
+    href: '/dashboard',
     label: 'Dashboard',
+    svg: (
+      <>
+        <svg
+          className="-mr-1 h-5 w-5 "
+          viewBox="0 0 20 20"
+          fill="currentColor"
+          aria-hidden="true"
+        >
+          <path
+            fillRule="evenodd"
+            d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z"
+            clipRule="evenodd"
+          />
+        </svg>
+      </>
+    ),
+  },
+  {
+    href: '/dashboard/principal',
+    label: 'Principal',
   },
   {
     href: '/dashboard/contas',
@@ -34,8 +54,8 @@ export function AsideNavbar() {
           />
         </Link>
         <ul>
-          {links.map(({ href, label }) => (
-            <Li key={href} href={href} path={path}>
+          {links.map(({ href, label, svg }) => (
+            <Li key={href} href={href} path={path} svg={svg}>
               {label}
             </Li>
           ))}
